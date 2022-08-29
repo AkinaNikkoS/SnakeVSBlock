caf22afc6dd86c5340bf4c33c36cda9dc24652ee
AkinaNikkoS <akinanikko@mail.ru>
2022-08-29 16:57:29 +0300

    MoveSnake

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
 
     public float Speed;
-    public Rigidbody Rigidbody;
     public float Sensitivity;
+    public Transform SnakeHead;
+    public int Health = 3;
+    public int Length = 3;
+    public TextMeshPro PointsText;
+    
 
     private Vector3 _previousMousePosition;
+    private Rigidbody Rigidbody;
+    private Vector3 tempVect = new Vector3(0, 0, 1);
 
-
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        PointsText.SetText(Health.ToString());
+    }
     public void Movement()
     {
         Rigidbody.velocity = new Vector3(0, 0, Speed);
@@ -19,8 +30,19 @@ public class Player : MonoBehaviour
 
     private void Update()
     { 
-        Movement(); 
-            
+        tempVect = tempVect.normalized* Speed * Time.deltaTime;
+        Rigidbody.MovePosition(transform.position + tempVect);
+
+        if (Input.GetMouseButton(0))
+        {
+
+            Vector3 moveSide = Input.mousePosition - _previousMousePosition;
+            moveSide = moveSide.normalized* Speed * Time.deltaTime;
+            Vector3 newPosition = new Vector3(transform.position.x + moveSide.x, 0, transform.position.z + tempVect.z);
+            Rigidbody.MovePosition(newPosition);
+        }
+
+        _previousMousePosition = Input.mousePosition;
     }
 
 
