9a778690d464cc5a1d7fb9bf0306289e52e4580c
AkinaNikkoS <akinanikko@mail.ru>
2022-09-07 11:34:51 +0300

    WorkingVersion

--- Assets/Scripts/GameResult.cs
+++ Assets/Scripts/GameResult.cs
@@ -5,6 +5,7 @@ public class GameResult : MonoBehaviour
 {
     public Player Controls;
     public GameObject Loss;
+    public GameObject Win;
 
     public enum State
     {
@@ -13,8 +14,13 @@ public class GameResult : MonoBehaviour
         Loss,
     }
 
+    private AudioSource _audio;
     public State CurrentState { get; private set; }
 
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
     public void OnPlayerDied()
     {
         if (CurrentState != State.Playing) return;
@@ -22,6 +28,7 @@ public class GameResult : MonoBehaviour
         Controls.enabled = false;
         Debug.Log("Game Over!");
         Loss.SetActive(true);
+        _audio.Stop();
     }
 
     public void OnPlayerWon()
@@ -30,5 +37,29 @@ public class GameResult : MonoBehaviour
         CurrentState = State.Won;
         Controls.enabled = false;
         Debug.Log("You Won!");
+        Win.SetActive(true);
+        _audio.Stop();
+    }
+
+    public void OnPlayerRestart()
+    {
+        if (CurrentState != State.Loss) return;
+        Loss.SetActive(false);
+        ReloadLevel();
+        _audio.Play();
+    }
+    public void OnPlayerNextLevel()
+    {
+        if (CurrentState != State.Won) return;
+        Win.SetActive(false);
+        ReloadLevel();
+        _audio.Play();
+        CurrentState = State.Playing;
+        Controls.enabled = true;
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -73,8 +71,8 @@ public class Player : MonoBehaviour
             Value = collision.gameObject.GetComponent<Block>().Value;
             if (Value >= Health)
             {
-                GameResult.OnPlayerDied();
                 Rigidbody.velocity = Vector3.zero;
+                GameResult.OnPlayerDied();
             }
             else
             {
